b240ac19b9ad919aad53e5127a95609a9675cca2
ktcindy <shaohua0611@gmail.com>
2024-04-02 22:03:32 +0800

    feat: 添加页面

--- src/views/pages/come.vue
+++ src/views/pages/come.vue
@@ -6,12 +6,30 @@ const showPopover = ref(false)
 // 通过 actions 属性来定义菜单选项
 const actions = [{ text: '编辑' }, { text: '清空' }, { text: '隐藏' }]
 const onSelect = (action) => showToast(action.text)
-const historyItem=ref([
+const historyItem = ref([
   {
-    url:'https://fastly.jsdelivr.net/npm/@vant/assets/cat.jpeg',
-    name:'口袋海贼王',
-    title:'2013最热海贼手机游戏，口袋海贼王',
-    count:'关注 3.9万'
+    url: 'https://fastly.jsdelivr.net/npm/@vant/assets/cat.jpeg',
+    name: '口袋海贼王',
+    title: '2013最热海贼手机游戏，口袋海贼王',
+    count: '关注 3.9万'
+  },
+  {
+    url: 'https://fastly.jsdelivr.net/npm/@vant/assets/cat.jpeg',
+    name: '口袋海贼王',
+    title: '2013最热海贼手机游戏，口袋海贼王',
+    count: '关注 3.9万'
+  },
+  {
+    url: 'https://fastly.jsdelivr.net/npm/@vant/assets/cat.jpeg',
+    name: '口袋海贼王',
+    title: '2013最热海贼手机游戏，口袋海贼王',
+    count: '关注 3.9万'
+  },
+  {
+    url: 'https://fastly.jsdelivr.net/npm/@vant/assets/cat.jpeg',
+    name: '口袋海贼王',
+    title: '2013最热海贼手机游戏，口袋海贼王',
+    count: '关注 3.9万'
   }
 ])
 </script>
@@ -33,16 +51,11 @@ const historyItem=ref([
         </van-popover>
       </div>
       <div class="history_bar">
-        <div class="history_bar_item" v-for="(item,index) in historyItem" :key="index">
-          <van-image
-            round
-            width="3rem"
-            height="3rem"
-            :src="item.url"
-          />
-          <div>{{item.name}}</div>
-          <div>{{item.title}}</div>
-          <div>{{item.count}}</div>
+        <div class="history_bar_item" v-for="(item, index) in historyItem" :key="index">
+          <van-image round width="3rem" height="3rem" :src="item.url" />
+          <div class="history_bar_item_name">{{ item.name }}</div>
+          <div class="history_bar_item_title">{{ item.title }}</div>
+          <div class="history_bar_item_count">{{ item.count }}</div>
         </div>
       </div>
     </Content>
@@ -55,14 +68,35 @@ const historyItem=ref([
   margin: var(--padding-1);
 }
 .history_bar {
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  grid-column-gap: 20px;
-  grid-row-gap: 20px;
+  //   display: grid;
+  //   grid-template-columns: 1fr 1fr 1fr;
+  //   grid-column-gap: 20px;
+  //   grid-row-gap: 20px;
+  display: flex;
+  width: 100vw;
   .history_bar_item {
     padding: var(--padding-1);
     font-size: var(--size-1);
     text-align: center;
+    background-color: var(--color-0);
+    width: 30%;
+    .history_bar_item_name {
+      font-size: 14px;
+      padding-bottom: 10px;
+    }
+    .history_bar_item_title {
+      font-size: 10px;
+      padding-bottom: 10px;
+      overflow: hidden;
+      //两行溢出省略号
+      text-overflow: ellipsis;
+      display: -webkit-box;
+      -webkit-line-clamp: 2;
+      -webkit-box-orient: vertical;
+    }
+    .history_bar_item_count {
+      font-size: 12px;
+    }
   }
 }
 </style>
